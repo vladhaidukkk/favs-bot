e13045bc8e3fa030580f1a7c9cd8474959b8e916
Vladyslav Haiduk <the.vladhaiduk@gmail.com>
2024-07-23 21:25:19 +0300

    create simple echo messages handler

--- main.py
+++ main.py
@@ -2,7 +2,7 @@ import asyncio
 import logging
 import os
 
-from aiogram import Bot, Dispatcher
+from aiogram import Bot, Dispatcher, types
 
 # To see INFO aiogram logs in the console.
 logging.basicConfig(level=logging.INFO)
@@ -11,6 +11,11 @@ BOT_TOKEN = os.environ["BOT_TOKEN"]
 
 bot = Bot(token=BOT_TOKEN)
 dp = Dispatcher()
+
+
+@dp.message()
+async def echo(message: types.Message):
+    await message.answer(text=message.text)
 
 
 async def main():
