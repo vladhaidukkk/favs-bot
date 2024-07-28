d106ffed9216bb844bc220d000b29beb19d0e990
Vladyslav Haiduk <the.vladhaiduk@gmail.com>
2024-07-28 20:32:05 +0300

    set up sqlalchemy core abstractions & migrations

---
+++ bot/db/core.py
@@ -0,0 +1,6 @@
+from sqlalchemy.ext.asyncio import async_sessionmaker, create_async_engine
+
+from bot.config import settings
+
+engine = create_async_engine(url=settings.db.url, echo=True, echo_pool=True)
+session_factory = async_sessionmaker(engine, autoflush=False, expire_on_commit=False)
