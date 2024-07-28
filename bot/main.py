9122062750419d7ecb7334e93db52c1f85991df8
Vladyslav Haiduk <the.vladhaiduk@gmail.com>
2024-07-28 20:54:52 +0300

    save user into database on start command

--- bot/main.py
+++ bot/main.py
@@ -3,8 +3,11 @@ import logging
 
 from aiogram import Bot, Dispatcher, types
 from aiogram.filters import CommandStart
+from sqlalchemy import insert, select
 
 from bot.config import settings
+from bot.db.core import session_factory
+from bot.db.models import UserModel
 
 # To see INFO aiogram logs in the console.
 logging.basicConfig(level=logging.INFO)
@@ -15,6 +18,15 @@ dp = Dispatcher()
 
 @dp.message(CommandStart())
 async def handle_start(message: types.Message):
+    async with session_factory() as session:
+        query = select(UserModel).filter_by(telegram_id=message.from_user.id)
+        result = await session.execute(query)
+        existing_user = result.scalar_one_or_none()
+        if not existing_user:
+            stmt = insert(UserModel).values(telegram_id=message.from_user.id)
+            await session.execute(stmt)
+            await session.commit()
+
     await message.answer(f"Hello, {message.from_user.full_name}!")
 
 
